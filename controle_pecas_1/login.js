8ed3a1e7070a867783a7a3040509657542d6e10d
George Mendes Marra <142532721+GeorgeMendesMarra@users.noreply.github.com>
2026-03-15 16:06:32 -0300

    Update login.js

--- controle_pecas_1/login.js
+++ controle_pecas_1/login.js
@@ -1,1 +1,17 @@
+function login(){
 
+let usuario = document.getElementById("usuario").value;
+let senha = document.getElementById("senha").value;
+
+if(usuario === "admin" && senha === "123"){
+    
+localStorage.setItem("logado", "true");
+window.location.href = "index.html";
+
+}else{
+
+document.getElementById("erro").innerText = "Usuário ou senha incorretos";
+
+}
+
+}
